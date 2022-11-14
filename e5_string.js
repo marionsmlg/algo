4d0ef8fa01c8422c5dedabc781428a4a0dc118a9
marionsmlg <marion.schimmerling2@hotmail.fr>
2022-11-14 10:37:29 +0100

    algoEau

---
+++ e5_string.js
@@ -0,0 +1,26 @@
+// Créez un programme qui détermine si une chaîne de caractère se trouve dans une autre.
+// Afficher error et quitter le programme en cas de problèmes d’arguments.
+
+// $ node e5_string.js bonjour jour
+// true
+
+// $ node e5_string.js bonjour soir
+// false
+
+// $ node e5_string.js hey
+// error
+
+let args = process.argv.slice(2);
+let arg1 = args[0];
+let arg2 = args[1];
+
+function includesString(str1, str2) {
+  if (args.length !== 2) {
+    return "error";
+  }
+  if (str1.includes(str2)) {
+    return true;
+  }
+  return false;
+}
+console.log(includesString(arg1, arg2));
