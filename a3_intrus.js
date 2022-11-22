7f900d376dd0eec4fa194729419e9cecfb651973
marionsmlg <marion.schimmerling2@hotmail.fr>
2022-11-22 12:39:07 +0100

    algoAir

---
+++ a3_intrus.js
@@ -0,0 +1,29 @@
+// Créez un programme qui retourne la valeur qui n’a pas de paire.
+
+// $ node a3_intrus.js carreau trefle trefle coeur carreau
+// Intrus détecté: coeur
+
+// $ node a3_intrus.js 1 2 3 4 5 4 3 2 1
+// Intrus détecté: 5
+
+// $ node a3_intrus.js 1 2 1 2
+// Aucun intrus détecté
+
+let args = process.argv.slice(2);
+
+// function isSingle(arr) {
+//   return arr.find((x) => arr.indexOf(x) === arr.lastIndexOf(x));
+// }
+
+function isSingle(arr) {
+  return arr.some((x) => arr.indexOf(x) === arr.lastIndexOf(x));
+}
+
+function hasIntruder(arr) {
+  let intruder = arr.find((x) => arr.indexOf(x) === arr.lastIndexOf(x));
+  if (isSingle(arr)) {
+    return `Intrus détecté: ${intruder}`;
+  }
+  return "Aucun intrus détecté";
+}
+console.log(hasIntruder(args));
